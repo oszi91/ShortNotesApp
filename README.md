0b687249d26f8497fade016885e1fc90d3059b34
oszi91 <69679873+oszi91@users.noreply.github.com>
2021-10-23 19:24:36 +0200

    add readme / code formatting

---
+++ README.md
@@ -0,0 +1,19 @@
+# Short Notes App
+![](ghPreview/1.png)
+
+## Description:
+"Short Notes App" is an app where you can add and delete your notes, mark them as finished or unfinished. You can also filter them based on that. To see the progress of your tasks you can use statistics and a bar that shows you in % how much you have left to do. All data is saved in local storage, so when you leave the page and enter it again it will remain saved.
+
+## Live Demo
+[Short Notes App - live preview](https://oszi91.github.io/ShortNotesApp/)
+
+## Technologies and additional dependencies:
+| Tool | Description |
+| :-------------:|--------------|
+| HTML5 | A markup language used for structuring, and presenting content on the World Wide Web. |
+| CSS | A simple mechanism for adding style to Web documents. |
+| Vanilla JavaScript | Pure JavaScript. |
+
+## Installation:
+
+-  ```git clone https://github.com/oszi91/ShortNotesApp.git``` to clone the repository and view the website in the browser.
--- app.js
+++ app.js
@@ -1,43 +1,54 @@
-    const form = document.querySelector('.form');
-    const formTextInput = document.querySelector('.form__input');
-    const todoList = document.querySelector('.todo-list');
-    const selectToDo = document.querySelector('.form__select-input');
+const form = document.querySelector('.form');
+const formTextInput = document.querySelector('.form__input');
+const todoList = document.querySelector('.todo-list');
+const selectToDo = document.querySelector('.form__select-input');
 
-    const dropdownList = document.querySelector('.form__dropdown-list');
-    const dropdownMainOption = document.querySelector('.form__dropdown-main-option');
-    const dropdownContainer = document.querySelector('.form__dropdown-container');
+const dropdownList = document.querySelector('.form__dropdown-list');
+const dropdownMainOption = document.querySelector(
+	'.form__dropdown-main-option'
+);
+const dropdownContainer = document.querySelector('.form__dropdown-container');
 
-    const error = document.querySelector('.form__error');
-    const allTasksText = document.querySelector('.header__all-tasks > span');
-    const completedTasksText = document.querySelector('.header__completed-tasks > span');
-    const uncompletedTasksText = document.querySelector('.header__uncompleted-tasks > span');
+const error = document.querySelector('.form__error');
+const allTasksText = document.querySelector('.header__all-tasks > span');
+const completedTasksText = document.querySelector(
+	'.header__completed-tasks > span'
+);
+const uncompletedTasksText = document.querySelector(
+	'.header__uncompleted-tasks > span'
+);
 
-    const progressPercent = document.querySelector('.header__progress-bar--percent');
-    const completedTasks = document.getElementsByClassName('todo-list__item-name--done');
+const progressPercent = document.querySelector(
+	'.header__progress-bar--percent'
+);
+const completedTasks = document.getElementsByClassName(
+	'todo-list__item-name--done'
+);
 
-    let tasks = [];
+let tasks = [];
 
-    getTasksFromLSCheck();
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+getTasksFromLSCheck();
+localStorage.setItem('tasks', JSON.stringify(tasks));
 
-    const updateBarAndTasks = () => {
-        completedTasksText.textContent = completedTasks.length;
-        uncompletedTasksText.textContent = tasks.length - Number(completedTasks.length);
+const updateBarAndTasks = () => {
+	completedTasksText.textContent = completedTasks.length;
+	uncompletedTasksText.textContent =
+		tasks.length - Number(completedTasks.length);
 
-        let progress = `${Math.floor((completedTasks.length/tasks.length) *100)}%`;
-        progressPercent.style.width = progress;
-        progressPercent.textContent = progress;
+	let progress = `${Math.floor((completedTasks.length / tasks.length) * 100)}%`;
+	progressPercent.style.width = progress;
+	progressPercent.textContent = progress;
 
-        if (completedTasks.length === 0) {
-            progressPercent.style.width = '10%';
-            progressPercent.textContent = '0%';
-        }
-    };
+	if (completedTasks.length === 0) {
+		progressPercent.style.width = '10%';
+		progressPercent.textContent = '0%';
+	}
+};
 
-    const addTask = task => {
-        const todoItem = document.createElement('div');
-        todoItem.classList.add('todo-list__item-container');
-        todoItem.innerHTML = ` 
+const addTask = task => {
+	const todoItem = document.createElement('div');
+	todoItem.classList.add('todo-list__item-container');
+	todoItem.innerHTML = ` 
     <li class="todo-list__item-name">${task}</li>
     <button class="todo-list__item-complete"><i class="fas fa-check"></i></button>
     <button class="todo-list__item-delete"><i class="fas fa-times"></i></button>
@@ -45,132 +56,132 @@
     <i class="fas fa-map-pin"></i>
     </div>
     `;
-        todoList.appendChild(todoItem);
-        tasks.push({
-            task: task,
-            done: false
-        });
-        allTasksText.textContent = tasks.length;
-    };
+	todoList.appendChild(todoItem);
+	tasks.push({
+		task: task,
+		done: false,
+	});
+	allTasksText.textContent = tasks.length;
+};
 
-    const addTaskToList = e => {
-        e.preventDefault();
-        const task = formTextInput.value;
+const addTaskToList = e => {
+	e.preventDefault();
+	const task = formTextInput.value;
 
-        if (task.length > 55) {
-            error.textContent = `Note can't be longer than 55 signs`;
-        } else if (!task) {
-            error.textContent = `This field can't be empty`;
-        } else {
-            error.textContent = '';
-            
-            addTask(task);
-            saveTaskToLocalStorage(task, false);
+	if (task.length > 55) {
+		error.textContent = `Note can't be longer than 55 signs`;
+	} else if (!task) {
+		error.textContent = `This field can't be empty`;
+	} else {
+		error.textContent = '';
 
-            formTextInput.value = '';
+		addTask(task);
+		saveTaskToLocalStorage(task, false);
 
-            updateBarAndTasks();
-            window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
-        }
-    };
+		formTextInput.value = '';
 
-    const deleteOrCompleteTask = e => {
-        const task = e.target;
+		updateBarAndTasks();
+		window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+	}
+};
 
-        if (task.classList.contains('todo-list__item-complete')) {
-            const taskContainer = task.parentElement;
-            const completeTask = taskContainer.children[0];
-            const taskName = completeTask.textContent;
-            const getIndexOfCompleteTask = tasks.findIndex(t => t.task === taskName);
+const deleteOrCompleteTask = e => {
+	const task = e.target;
 
-            completeTask.classList.toggle('todo-list__item-name--done');
-            taskContainer.classList.toggle('todo-list__item-container--opacity');
+	if (task.classList.contains('todo-list__item-complete')) {
+		const taskContainer = task.parentElement;
+		const completeTask = taskContainer.children[0];
+		const taskName = completeTask.textContent;
+		const getIndexOfCompleteTask = tasks.findIndex(t => t.task === taskName);
 
-            let isDone;
-            if (tasks) {
-                if (completeTask.classList.contains('todo-list__item-name--done')) {
-                    isDone = tasks[getIndexOfCompleteTask].done = true;
-                } else {
-                    isDone = tasks[getIndexOfCompleteTask].done = false;
-                }
-            }
+		completeTask.classList.toggle('todo-list__item-name--done');
+		taskContainer.classList.toggle('todo-list__item-container--opacity');
 
-            saveIsDoneToLS(taskName, isDone);
-            updateBarAndTasks();
-        };
+		let isDone;
+		if (tasks) {
+			if (completeTask.classList.contains('todo-list__item-name--done')) {
+				isDone = tasks[getIndexOfCompleteTask].done = true;
+			} else {
+				isDone = tasks[getIndexOfCompleteTask].done = false;
+			}
+		}
 
-        if (task.classList.contains('todo-list__item-delete')) {
-            const taskToDelete = task.parentElement;
-            const taskName = taskToDelete.textContent;
-            const getIndexOfTaskToDelete = tasks.findIndex(t => t.task === taskName);
-            tasks.splice(getIndexOfTaskToDelete, 1);
-            
-            taskToDelete.remove();
-            deleteTaskFromLocalStorage(taskToDelete);
+		saveIsDoneToLS(taskName, isDone);
+		updateBarAndTasks();
+	}
 
-            updateBarAndTasks();
-        };
-    };
+	if (task.classList.contains('todo-list__item-delete')) {
+		const taskToDelete = task.parentElement;
+		const taskName = taskToDelete.textContent;
+		const getIndexOfTaskToDelete = tasks.findIndex(t => t.task === taskName);
+		tasks.splice(getIndexOfTaskToDelete, 1);
 
-    const dropdownIsOpen = () => {
-        const dropdownBtn = document.querySelector('.form__dropdown-btn');
-        dropdownList.classList.toggle('form__dropdown-btn--is-open');
-        dropdownBtn.classList.toggle('form__dropdown-btn--rotate-arrow');
-    };
+		taskToDelete.remove();
+		deleteTaskFromLocalStorage(taskToDelete);
 
-    const selectTypeOfTasks = e => {
-        const selectTasks = [...todoList.children];
-        const typeOfTasks = e.target.dataset.value;
+		updateBarAndTasks();
+	}
+};
 
-        selectTasks.forEach(task => {
-            const taskComplete = task.children[0];
+const dropdownIsOpen = () => {
+	const dropdownBtn = document.querySelector('.form__dropdown-btn');
+	dropdownList.classList.toggle('form__dropdown-btn--is-open');
+	dropdownBtn.classList.toggle('form__dropdown-btn--rotate-arrow');
+};
 
-            switch (typeOfTasks) {
-                case 'all':
-                    dropdownMainOption.textContent = 'All';
-                    task.style.display = 'flex';
-                    break;
-                case 'completed':
-                    dropdownMainOption.textContent = 'Completed';
-                    if (taskComplete.classList.contains('todo-list__item-name--done')) {
-                        task.style.display = 'flex'
-                    } else {
-                        task.style.display = 'none'
-                    }
-                    break;
-                case 'uncompleted':
-                    dropdownMainOption.textContent = 'Uncompleted';
-                    if (!taskComplete.classList.contains('todo-list__item-name--done')) {
-                        task.style.display = 'flex'
-                    } else {
-                        task.style.display = 'none'
-                    }
-                    break;
-            }
-        })
-    };
+const selectTypeOfTasks = e => {
+	const selectTasks = [...todoList.children];
+	const typeOfTasks = e.target.dataset.value;
 
-    dropdownList.addEventListener('click', selectTypeOfTasks);
+	selectTasks.forEach(task => {
+		const taskComplete = task.children[0];
 
-    // Local Storage
+		switch (typeOfTasks) {
+			case 'all':
+				dropdownMainOption.textContent = 'All';
+				task.style.display = 'flex';
+				break;
+			case 'completed':
+				dropdownMainOption.textContent = 'Completed';
+				if (taskComplete.classList.contains('todo-list__item-name--done')) {
+					task.style.display = 'flex';
+				} else {
+					task.style.display = 'none';
+				}
+				break;
+			case 'uncompleted':
+				dropdownMainOption.textContent = 'Uncompleted';
+				if (!taskComplete.classList.contains('todo-list__item-name--done')) {
+					task.style.display = 'flex';
+				} else {
+					task.style.display = 'none';
+				}
+				break;
+		}
+	});
+};
 
-    function getTasksFromLSCheck() {
-        const getTasks = localStorage.getItem('tasks');
+dropdownList.addEventListener('click', selectTypeOfTasks);
 
-        if (!getTasks) {
-            tasks = [];
-        } else {
-            tasks = JSON.parse(getTasks)
-        };
-    };
+// Local Storage
 
-    const loadTaskList = () => {
-        tasks.forEach(allTasks => {
-            const todoItem = document.createElement('div');
-            
-            if (allTasks.done === false) {
-                todoItem.classList.add('todo-list__item-container');
-                todoItem.innerHTML = ` 
+function getTasksFromLSCheck() {
+	const getTasks = localStorage.getItem('tasks');
+
+	if (!getTasks) {
+		tasks = [];
+	} else {
+		tasks = JSON.parse(getTasks);
+	}
+}
+
+const loadTaskList = () => {
+	tasks.forEach(allTasks => {
+		const todoItem = document.createElement('div');
+
+		if (allTasks.done === false) {
+			todoItem.classList.add('todo-list__item-container');
+			todoItem.innerHTML = ` 
                 <li class="todo-list__item-name">${allTasks.task}</li>
                 <button class="todo-list__item-complete"><i class="fas fa-check"></i></button>
                 <button class="todo-list__item-delete"><i class="fas fa-times"></i></button>
@@ -178,9 +189,12 @@
                 <i class="fas fa-map-pin"></i>
                 </div>
                 `;
-            } else {
-                todoItem.classList.add('todo-list__item-container', 'todo-list__item-container--opacity');
-                todoItem.innerHTML = ` 
+		} else {
+			todoItem.classList.add(
+				'todo-list__item-container',
+				'todo-list__item-container--opacity'
+			);
+			todoItem.innerHTML = ` 
                 <li class="todo-list__item-name todo-list__item-name--done">${allTasks.task}</li>
                 <button class="todo-list__item-complete"><i class="fas fa-check"></i></button>
                 <button class="todo-list__item-delete"><i class="fas fa-times"></i></button>
@@ -188,65 +202,71 @@
                 <i class="fas fa-map-pin"></i>
                 </div>
                 `;
-            };
+		}
 
-            todoList.appendChild(todoItem);
-        });
-    }
+		todoList.appendChild(todoItem);
+	});
+};
 
-    const getTasksFromLocalStorage = () => {
+const getTasksFromLocalStorage = () => {
+	// Test tasks for a new visitors
+	if (localStorage.getItem('tasks') === '[]') {
+		localStorage.setItem(
+			'tasks',
+			JSON.stringify([
+				{ task: 'Test Task Done', done: true },
+				{ task: 'Test Task', done: false },
+			])
+		);
+		tasks.push(
+			{ task: 'Test Task Done', done: true },
+			{ task: 'Test Task', done: false }
+		);
+		loadTaskList();
+	} else {
+		loadTaskList();
+	}
 
-        // Test tasks for a new visitors
-        if (localStorage.getItem('tasks') === "[]") {
-            localStorage.setItem('tasks', JSON.stringify([{"task":"Test Task Done","done":true}, {"task":"Test Task","done":false}]));
-            tasks.push({task:"Test Task Done",done:true}, {task:"Test Task",done:false});
-            loadTaskList();
-          } else{
-            loadTaskList();
-          }
+	allTasksText.textContent = tasks.length;
+	updateBarAndTasks();
+};
 
-        allTasksText.textContent = tasks.length;
-        updateBarAndTasks();
-    };
+const saveTaskToLocalStorage = (taskName, isDone) => {
+	getTasksFromLSCheck();
 
-    const saveTaskToLocalStorage = (taskName, isDone) => {
-        getTasksFromLSCheck();
+	tasks.push({
+		task: taskName,
+		done: isDone,
+	});
 
-        tasks.push({
-            task: taskName,
-            done: isDone
-        });
+	let getIndexOfCompleteTask = tasks.findIndex(t => t.task === taskName);
+	tasks[getIndexOfCompleteTask].done = isDone;
 
-        let getIndexOfCompleteTask = tasks.findIndex(t => t.task === taskName);
-        tasks[getIndexOfCompleteTask].done = isDone;
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    };
+const saveIsDoneToLS = (taskName, isDone) => {
+	getTasksFromLSCheck();
 
-    const saveIsDoneToLS = (taskName, isDone) => {
-        getTasksFromLSCheck();
+	let getIndexOfCompleteTask = tasks.findIndex(t => t.task === taskName);
+	tasks[getIndexOfCompleteTask].done = isDone;
 
-        let getIndexOfCompleteTask = tasks.findIndex(t => t.task === taskName);
-        tasks[getIndexOfCompleteTask].done = isDone;
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    };
+const deleteTaskFromLocalStorage = taskName => {
+	getTasksFromLSCheck();
 
-    const deleteTaskFromLocalStorage = taskName => {
-        getTasksFromLSCheck();
+	const taskToDelete = taskName.children[0].textContent;
+	const getIndexOfTaskToDelete = tasks.findIndex(t => t.task === taskToDelete);
 
-        const taskToDelete = taskName.children[0].textContent;
-        const getIndexOfTaskToDelete = tasks.findIndex(t => t.task === taskToDelete);
+	tasks.splice(getIndexOfTaskToDelete, 1);
+	allTasksText.textContent = tasks.length;
 
-        tasks.splice(getIndexOfTaskToDelete, 1);
-        allTasksText.textContent = tasks.length;
+	localStorage.setItem('tasks', JSON.stringify(tasks));
+};
 
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    };
-
-    document.addEventListener('DOMContentLoaded', getTasksFromLocalStorage);
-    form.addEventListener('submit', addTaskToList);
-    todoList.addEventListener('click', deleteOrCompleteTask);
-    dropdownContainer.addEventListener('click', dropdownIsOpen);
-    
-
+document.addEventListener('DOMContentLoaded', getTasksFromLocalStorage);
+form.addEventListener('submit', addTaskToList);
+todoList.addEventListener('click', deleteOrCompleteTask);
+dropdownContainer.addEventListener('click', dropdownIsOpen);
